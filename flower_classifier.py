9f581fb58b4be3f4f832cf4d0c8f9bb9238197f5
Nahiyan Mubashshir <nahiyanmubashshir@gmail.com>
2021-05-10 20:48:59 +0600

    Initial commit

---
+++ flower_classifier.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 00:56:13 2021
+
+@author: tns
+"""
+
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
+import matplotlib.pyplot as plt
+
+import tflearn.datasets.oxflower17 as oxflower17
+X, Y = oxflower17.load_data(one_hot=True, resize_pics=(227, 227))
+
+epochs = 100
+
+# Building 'AlexNet'
+network = input_data(shape=[None, 227, 227, 3])
+network = conv_2d(network, 96, 11, strides=4, activation='relu')
+network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = conv_2d(network, 256, 5, activation='relu')
+network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = conv_2d(network, 384, 3, activation='relu')
+network = conv_2d(network, 384, 3, activation='relu')
+network = conv_2d(network, 256, 3, activation='relu')
+network = max_pool_2d(network, 3, strides=2)
+network = local_response_normalization(network)
+network = fully_connected(network, 4096, activation='tanh')
+network = dropout(network, 0.5)
+network = fully_connected(network, 4096, activation='tanh')
+network = dropout(network, 0.5)
+network = fully_connected(network, 17, activation='softmax')
+network = regression(network, optimizer='momentum',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Training
+model = tflearn.DNN(network, checkpoint_path='model_alexnet',
+                    max_checkpoints=1, tensorboard_verbose=2)
+history = model.fit(X, Y, n_epoch=epochs, validation_set=0.1, shuffle=True,
+          show_metric=True, batch_size=64, snapshot_step=20,
+          snapshot_epoch=False, run_id='alexnet_oxflowers17')
+
+model.summary()
+
+def Res_Plot_Test_Save(name):
+    print("[INFO] Plotting model accuracy and Loss")
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    #Train and validation accuracy
+    plt.plot(epochs, acc, 'b', label='Training accurarcy')
+    plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
+    plt.title('Training and Validation accurarcy')
+    plt.legend()
+
+    plt.figure()
+    #Train and validation loss
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and Validation loss')
+    plt.legend()
+    plt.show()
+    
+    print("[INFO] Calculating model accuracy")
+    scores = model.evaluate(X, Y)
+    print(f"Test Accuracy: {scores[1]*100}")
+    
+    print("[INFO] Saving model")
+    model.save(name)
+
+Res_Plot_Test_Save('result.h5')
